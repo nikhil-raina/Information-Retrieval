7877ef149946832c54f6828cad1f5c092e99f794
onkarsawant <ops8383@g.rit.edu>
2020-11-06 18:09:32 -0500

    added code creat document dictionary

--- project2/project2.py
+++ project2/project2.py
@@ -1,12 +1,32 @@
 from bs4 import BeautifulSoup
 
 
-f = open('p2-data/All Transcripts - My Little Pony Friendship is Magic Wiki.html')
+documents = {} #key=doc; value=document
+f = open('project2/p2-data/All Transcripts - My Little Pony Friendship is Magic Wiki.html')
 
 soup = BeautifulSoup(f.read(), 'html.parser')
 
-table_a_tags = soup.findAll(id='mw-content-text')
+tags = soup.find(id='mw-content-text').find('p').contents
 
-for a in table_a_tags:
-    print(a.prettify())
 
+
+currDoc = None
+i=0
+while i < len(tags):
+    if tags[i] != '\n':
+        if tags[i].name == 'h2':
+            currDoc = tags[i].text
+            documents[currDoc] = []
+        elif currDoc != None:
+            if tags[i].name == 'table':
+                attr =  tags[i].attrs
+                if 'class' in  attr.keys() and attr['class'] == 'navbox':
+                    break
+                
+            documents[currDoc].append(tags[i].text)
+    i += 1
+print(documents) #getting 243 documents
+
+
+
+        
