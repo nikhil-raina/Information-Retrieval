07448c87f418e9e3635fee5edc2bd7ddf00371ba
onkarsawant <ops8383@g.rit.edu>
2020-11-07 17:59:59 -0500

    added indexing

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,10 @@
 from bs4 import BeautifulSoup
+from nltk.stem import PorterStemmer
+from string import punctuation
+import re
+import json
+
+
 
 def loadFile():
     documents = {} #key=doc; value=document
@@ -22,3 +28,37 @@ def loadFile():
                 documents[currDoc].append(tags[i].text)
         i += 1
     return documents
+
+
+
+
+
+def indexDocs():
+    docs = loadFile()
+    ps = PorterStemmer()
+    currDocId = 0
+    docIds = {}
+    docFrequencies = {}
+
+    for docName in docs.keys():
+        wordFrequency = {}
+        docFrequency = 0
+        docIds[currDocId] = docName
+        for line in docs[docName]:
+            tokens = re.split(r'\s+|['+punctuation+r']\s*', line.strip())
+            tokens = [ps.stem(token) for token in tokens]
+            for token in tokens:
+                if token != '':
+                    if token in wordFrequency.keys():
+                        wordFrequency[token]+=1
+                    else:
+                        wordFrequency[token] = 1
+                    docFrequency+=1
+        docFrequencies[currDocId] = {'doc':docFrequency, 'words':wordFrequency}
+
+        currDocId+=1
+    out = open('test.json','w')
+    json.dump(docFrequencies, out)
+
+
+indexDocs()
