b0510d13b02e0617528d35496b13b7e2836b6d55
onkarsawant <ops8383@g.rit.edu>
2020-11-07 20:54:51 -0500

    added tf-idf

---
+++ project2/preprocessing.py
@@ -0,0 +1,89 @@
+from bs4 import BeautifulSoup
+from nltk.stem import PorterStemmer
+from string import punctuation
+import re
+import json
+import math
+
+#using utf-8 format as the html files where encoded in that and has special chars that require it to be utf-8
+
+def loadFile():
+    documents = {} #key=doc; value=document
+    transcriptFile = open('project2/p2-data/All Transcripts - My Little Pony Friendship is Magic Wiki.html', encoding='utf-8')
+    soup = BeautifulSoup(transcriptFile.read(), 'html.parser')
+    transcriptFile.close()
+    tags = soup.find(id='mw-content-text').find('p').contents
+    currDoc = None
+    i=0
+    while i < len(tags):
+        if tags[i] != '\n':
+            if tags[i].name == 'h2':
+                currDoc = tags[i].text
+                documents[currDoc] = []
+            elif currDoc != None:
+                if tags[i].name == 'table':
+                    attr =  tags[i].attrs
+                    if 'class' in  attr.keys() and attr['class'] == 'navbox':
+                        break
+                documents[currDoc].append(tags[i].text)
+        i += 1
+    return documents
+
+
+def indexDocs():
+    docs = loadFile()
+    ps = PorterStemmer()
+    currDocId = 0
+    docIds = {}
+    termFrequency = {}
+    invertedIndex = {}
+    for docName in docs.keys():
+        wordFrequency = {}
+        wordsInDoc = 0.0
+        docIds[currDocId] = docName
+        for line in docs[docName]:
+            tokens = re.split(r'\s+|['+punctuation+r']\s*', line.strip())
+            for token in tokens:
+                token = ps.stem(token.lower())
+                if token != '':
+                    try:
+                        wordFrequency[token]+=1.0
+                    except:
+                        wordFrequency[token] = 1.0
+                    try:
+                        invertedIndex[token].add(currDocId)
+                    except:
+                        invertedIndex[token] = {currDocId}
+                    wordsInDoc+=1.0
+        for word in wordFrequency.keys():
+            wordFrequency[word] /= wordsInDoc 
+        termFrequency[currDocId] = {'doc':wordsInDoc, 'term':wordFrequency}
+
+        currDocId+=1
+    
+    inverseDocFrequency = {k: math.log(len(docIds)/(len(v)+1)) for k,v in invertedIndex.items()}
+    
+    tf_idf = {}
+
+    for id in docIds.keys():
+        for term in termFrequency[id]['term'].keys():
+            try:
+                tf_idf[id][term] = termFrequency[id]['term'][term] * inverseDocFrequency[term]
+            except:
+                tf_idf[id] = {term:termFrequency[id]['term'][term] * inverseDocFrequency[term]}
+    
+    out = open('tf.json','w', encoding='utf-8')
+    json.dump(termFrequency, out,ensure_ascii=False)
+    out.close()
+    out = open('idf.json','w',encoding='utf-8')
+    json.dump(inverseDocFrequency,out,ensure_ascii=False)
+    out.close()
+    out = open('tf_idf.json','w',encoding='utf-8')
+    json.dump(tf_idf,out,ensure_ascii=False)
+    out.close()
+
+
+
+
+    return termFrequency
+indexDocs()
--- project2/project2.py
+++ project2/project2.py
@@ -1,4 +1,29 @@
 import json 
 
-a = json.load(open('idf.json'))
-print(json.dumps(a))
+a = json.load(open('tf_idf.json'))
+
+def search(query):
+    try:
+        #finding the length of inverted lists for all the query words
+        inverted_list_len = []
+        for q in query:
+            inverted_list_len.append(len(inverted_list[q]))
+        zipped = zip(inverted_list_len, query)
+        #sorting the query words accoring to the inverted list length.
+        query = [x for _,x in sorted(zipped)]
+        #using the doc_list for the first query word
+        curr_docs = inverted_list[query[0]]
+        #looping through all other query words
+        for q in query[1:]:
+            q1Docs = curr_docs
+            q2Docs = inverted_list[q]
+            curr_docs = {}
+            #checks if both q1 and q2 docs have the documents in them 
+            for doc_id in q1Docs.keys():
+                if doc_id in q2Docs.keys():
+                    curr_docs[doc_id] = q1Docs[doc_id] + q2Docs[doc_id]
+        #sorting the documents 
+        curr_docs = {k : v for k, v in sorted(curr_docs.items(), key = lambda item: item[1], reverse=True)}    
+        return curr_docs    
+    except:
+        return {}
